570a27a45c9a1ee00e2575c0a404cd35cb6f3586
pixendrit <pixendrit@outlook.com>
2020-09-05 18:47:53 +0200

    logo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteTitle: 'Stina Städ', // Site title.
   siteTitleAlt: 'städtjänster', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteLogo: '/icons/Städtjänster.png', // Logo used for SEO and manifest.
   siteUrl: 'https://laughing-visvesvaraya-e9c3ff.netlify.app/', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: 'Vi hjälper dig med Flyttstädning, Hemstädning,  Visningsstäd ...', // Website description used for RSS feeds/meta description tag.
@@ -14,6 +14,6 @@ module.exports = {
   userLocation: 'Nygatan 7 31432 Hyltebruk, Sweden',
   userDescription: '',
   copyright: 'Copyright © pixendrit 2020. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  themeColor: '#841e71', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
